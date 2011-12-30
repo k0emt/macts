763d6cf96fd744e869ddce97e9c41c8658b2d5c2
Bryan Nehl <k0emt@dbbear.com>
2011-12-30 15:35:14 -0600

    Linear agent publishes to the direct exchange with topic of 'linear'

--- source/spikes/Aggregation/LinearAgent.py
+++ source/spikes/Aggregation/LinearAgent.py
@@ -4,23 +4,28 @@ import Infrastructure
 from Infrastructure import MessageContainer
 
 class LinearAgent:
-    def __init__(self):
-        print "Linear Agent"
-        self.counter = 0
-        print "Counter initialized to: ", self.counter
-
+    def Connect_RabbitMQ(self):
         print "Connecting to RabbitMQ...",
         # first thing this agent needs to do is connect to the tick exchange
         credentials = pika.PlainCredentials(Infrastructure.APP_USER,
-                                            Infrastructure.APP_PASS)
+            Infrastructure.APP_PASS)
         conn_params = pika.ConnectionParameters(
             host=Infrastructure.RABBITMQ_SERVER,
             virtual_host=Infrastructure.VHOST_AGGREGATE,
             credentials=credentials)
         conn = pika.BlockingConnection(conn_params)
         channel = conn.channel()
-
+        self.publishChannel = conn.channel()
         print "CONNECTED"
+        return channel
+
+    def __init__(self):
+        print "Linear Agent"
+        self.counter = 0
+        print "Counter initialized to: ", self.counter
+
+        channel = self.Connect_RabbitMQ()
+
         print "Creating Queue...",
         ourChan = channel.queue_declare(exclusive=True)
         channel.queue_bind(exchange=Infrastructure.EXCHANGE_SYSTEM_TICK,
@@ -46,7 +51,6 @@ class LinearAgent:
         channel.start_consuming()
         print "Linear Agent FINISHED"
 
-
     # callback that runs when message arrives -- see basic_consume() below
     def isStopProcessingMessage(self, body):
         return repr(Infrastructure.STOP_PROCESSING_MESSAGE) == body
@@ -59,7 +63,11 @@ class LinearAgent:
     # send transformed data to to Direct Exchange, "linear" topic
     # pull this out to Producer
     def sendMessage(self,message):
-        print ".....sendMessage ", message
+        print "SEND:", message,
+        self.publishChannel.basic_publish(exchange=Infrastructure.EXCHANGE_AGGREGATE,
+                                            routing_key='linear',
+                                            body=message)
+        print " SENT"
 
 if __name__ == "__main__":
     la = LinearAgent()
