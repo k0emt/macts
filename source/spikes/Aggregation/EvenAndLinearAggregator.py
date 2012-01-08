8ae2aee22e2e528db638a0ac8ef2c65697974286
Bryan Nehl <k0emt@dbbear.com>
2012-01-08 00:12:45 -0600

    added code to do a simple aggregation function

--- source/spikes/Aggregation/EvenAndLinearAggregator.py
+++ source/spikes/Aggregation/EvenAndLinearAggregator.py
@@ -27,8 +27,35 @@ class EvenAndLinearAggregator:
         print "CONNECTED"
         return channel
 
+    def add(self, incoming_json_message):
+        incoming = json.loads(incoming_json_message)
+        incoming_dictionary = dict(incoming[0])
+
+        # is there something in system_tick_dictionary with this key?
+        inbound_tick = incoming_dictionary[MessageContainer.SYSTEM_TICK_ID_FIELD]
+        inbound_value = incoming_dictionary[MessageContainer.SYSTEM_AGENT_VALUE_FIELD]
+        inbound_agent = incoming_dictionary[MessageContainer.SYSTEM_AGENT_FIELD]
+
+        print 'EAL inbound: %s %s %s' % (inbound_tick, inbound_value, inbound_agent)
+
+        if self.system_tick_dictionary.has_key(inbound_tick):
+            existing_value = self.system_tick_dictionary[inbound_tick]
+            del(self.system_tick_dictionary[inbound_tick])
+
+            # aggregate_result = existing_value + "|" + inbound_value
+            aggregate_result = long(existing_value) + long(inbound_value)
+
+            outGoingMessage = MessageContainer(inbound_tick,
+                                                EvenAndLinearAggregator.AGENT_NAME,
+                                                aggregate_result)
+            self.sendMessage(outGoingMessage.getJSON())
+        else:
+            print 'EAL adding new key'
+            self.system_tick_dictionary[inbound_tick] = inbound_value
+
     def __init__(self):
         print self.AGENT_NAME
+        self.system_tick_dictionary = {}
 
         channel = self.Connect_RabbitMQ()
 
@@ -45,8 +72,10 @@ class EvenAndLinearAggregator:
 
         def callback(ch, method, properties, body):
             print "RX {0} VIA {1} |".format(body, method.routing_key),
-            outGoingMessage = self.transform(body).getJSON()
-            self.sendMessage(outGoingMessage)
+            self.add(body)
+
+            #outGoingMessage = self.transform(body).getJSON()
+            #self.sendMessage(outGoingMessage)
 
         channel.basic_consume(callback, queue=queue_name, no_ack=True)
 
@@ -66,10 +95,10 @@ class EvenAndLinearAggregator:
 
     @staticmethod
     def transform(incoming_json_message):
-        # TODO aggregation code
         incoming = json.loads(incoming_json_message)
         incoming_dictionary = dict(incoming[0])
-        return MessageContainer(incoming_dictionary[MessageContainer.SYSTEM_TICK_ID_FIELD],
+        return MessageContainer(
+            incoming_dictionary[MessageContainer.SYSTEM_TICK_ID_FIELD],
             EvenAndLinearAggregator.AGENT_NAME,
             "{0}:{1}".format(
                 incoming_dictionary[MessageContainer.SYSTEM_AGENT_FIELD],
