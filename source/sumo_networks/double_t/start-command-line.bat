c9eab05043cc6ce2e55d0b5e899ef523c735e934
Bryan Nehl <k0emt@dbbear.com>
2012-03-14 12:19:00 -0500

    added link for launching dos prompt with correct path changed file name so that SUMO-GUI will see it

---
+++ source/sumo_networks/double_t/start-command-line.bat
@@ -0,0 +1,7 @@
+@echo off
+rem change the following line to set a new default dir:
+cd C:\sumo-0.14.0\bin\
+
+set default_dir=%windir%\..
+
+cmd /K "set PATH=%PATH%;%CD%&& set SUMO_BINDIR=%CD%&& set SUMO_HOME=%CD%\..&& cd /d %default_dir% && echo info: added location of sumo executables to the search path && echo info: variables SUMO_HOME, SUMO_BINDIR are set && cd /d C:\macts\source\sumo_networks\double_t
