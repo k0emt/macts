1a10f9dafa9989497e2979cf19827972866f07d6
Bryan Nehl <k0emt@dbbear.com>
2012-04-23 21:42:35 -0500

    add some verbose level output

--- source/macts/JSS_SafetyAgent.py
+++ source/macts/JSS_SafetyAgent.py
@@ -1,8 +1,9 @@
-import SafetyAgent
+from SafetyAgent import SafetyAgent
+from TrafficLightSignal import SignalPhase
 
 __author__ = 'k0emt'
 
-# TODO increment CommAgent EXPECTED_NUMBER_SAFETY_AGENT_COMMANDS ***********
+# NOTE increment Communications Agent EXPECTED_NUMBER_SAFETY_AGENT_COMMANDS
 
 class SafetyAgentStSaviours(SafetyAgent):
     """
@@ -10,6 +11,9 @@ class SafetyAgentStSaviours(SafetyAgent):
     B Avenue and B West Avenue.
     """
 
+    verbose_level = 0
+
+    # TODO
     # SR18a The safety agent examines the TLS plan to verify that there are no
     # simultaneously active paths that will cross each other in such a way as
     # to create an unsafe condition.
@@ -24,11 +28,20 @@ class SafetyAgentStSaviours(SafetyAgent):
     # SR19 If the plan is not safe,
     # it lets the planning agent know and provides the reason why.
 
-    # SR20 Submit verified safe plan to TLS command queue.
-    # sends: Name, Junction, Plan
+    def checkSafePlan(self, plan):
+        self.phase_manager.setPhase(plan)
 
-    def __init__(self):
+        self.verbose_display("SSSA %s %s %d",
+            (self.agent_name, self.simulationId, self.simulationStep),1)
+
+        # SR20 Submit verified safe plan to TLS command queue.
+        self.sendTrafficLightSignalCommand(self.phase_manager.current_phase)
+
+    def __init__(self, initial_phase):
         self.junction = "JunctionSS"
-
-if __name__ == "__main__":
-    SafetyAgentStSaviours()
+        self.agent_name = "SafetyAgentStSaviours"
+        self.Connect_RabbitMQ()
+        self.phase_manager = SignalPhase(initial_phase)
+        if self.phase_manager.status_last_change_request ==\
+           SignalPhase.STATUS_OK:
+            self.sendTrafficLightSignalCommand(self.phase_manager.current_phase)
